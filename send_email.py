9e5c99239bb780c9bdaea90bb74ef841c17cf9c7
ShreyaPrasad31 <s9prasad@edu.uwaterloo.ca>
2018-01-22 00:33:56 -0500

    send data back

--- send_email.py
+++ send_email.py
@@ -1,13 +1,14 @@
+
 from email.mimetext import MIMEText 
 import smtplib 
 
 def send_email(email, salary , average_salary,count):
 	from_email = shreyaprasad31121998@gmail.com
-	from_password = Sh31121998!
+	from_password = **********
 	to_email = email
 
 	subject = "Coop Data"
-	message = "Hey there, your entered data is <i>%s </i>.And average salary is <i> %s </i>. The sample size used is <i> %s </i>",  % (salary,average_salary,count)
+	message = "Hey there, your entered data is <i>%s </i>.And average salary is <i> %s </i>.The sample size used is <i> %s </i>",  % (salary,average_salary,count)
 
 	msg = MIMEText(message,'html')
     msg['Subject'] = subject
