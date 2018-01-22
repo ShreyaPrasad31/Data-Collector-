277e6cb73978b43f575ad7c9fe20a9d02c4b3ace
ShreyaPrasad31 <s9prasad@edu.uwaterloo.ca>
2018-01-22 00:31:36 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-rom flask import Flask, render_template, request
+from flask import Flask, render_template, request
 
-from flask.ext.sqlalchemy import SQLAlchemy 
+from flask.ext.sqlalchemy import SQLAlchemy
+from send_email import send_email 
+from sqlalchemy.sql import func
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:********@localhost/salary_collector'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:Sh31121998!@localhost/salary_collector'
 db = SQLAlchemy(app)
 
 class Data(db.Model):
@@ -28,14 +30,19 @@ def success():
 	if request.method == 'POST':
 		email = request.form["email_name"]
 		salary = request.form["salary_name"]
-		print(email,salary)
-		if db.session.query(Data).filter(Data.email == email):
+		
+		if db.session.query(Data).filter(Data.email == email).count()==0:
 
 			data = Data(email,salary)
 			db.session.add(data)
 			db.session.commit()
+			average_salary = db.session.query(func.avg(Data.salary))
+			average_salary = round(average_salary,1)
+			count = db.session.query(Data.salary).count()
+			send_email(email,salary,average_salary,count)
 			return render_template("success.html")
 
+ 	return render_template("index.html", text="Seems like we have gotten something from that email address already")
 
 if __name__=="__main__":
 	app.debug = True
